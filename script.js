801dcd021c13e52e17b33ddc2e45f943d839824b
Ying Jie Mei <jackymei95@gmail.com>
2025-09-16 12:11:30 -0400

    switchToDOMjs

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+const h1 = document.createElement("h1");
+const div = document.createElement("div");
+const input = document.createElement("input");
+const button = document.createElement("button");
 
+// title
+h1.textContent = "To-Do List";
+document.body.appendChild(h1);
+
+// div for taskInput
+input.id = "taskInput";
+input.type = "text";
+input.placeholder = "New task";
+
+button.id = "addTaskButton";
+button.textContent = "Add";
+div.append(input, button);
+document.body.appendChild(div);
