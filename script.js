fa0835c7bd0da86fa3c5a8fd4acf93aeb6fbd215
Ying Jie Mei <jackymei95@gmail.com>
2025-09-16 13:22:54 -0400

    addTask

--- script.js
+++ script.js
@@ -1,18 +1,48 @@
 const h1 = document.createElement("h1");
 const div = document.createElement("div");
-const input = document.createElement("input");
-const button = document.createElement("button");
+const taskInput = document.createElement("input");
+const addButton = document.createElement("button");
+const ul = document.createElement("ul");
+const li = document.createElement("li");
+const template = document.createElement("template");
+const span = document.createElement("span");
+const delButton = document.createElement("button");
 
 // title
 h1.textContent = "To-Do List";
 document.body.appendChild(h1);
 
 // div for taskInput
-input.id = "taskInput";
-input.type = "text";
-input.placeholder = "New task";
+taskInput.id = "taskInput";
+taskInput.type = "text";
+taskInput.placeholder = "New task";
 
-button.id = "addTaskButton";
-button.textContent = "Add";
-div.append(input, button);
+addButton.id = "addTaskButton";
+addButton.textContent = "Add";
+div.append(taskInput, addButton);
 document.body.appendChild(div);
+
+ul.id = "taskList";
+document.body.appendChild(ul);
+
+// template
+template.id = "SingleTaskTemplate";
+span.className = "task";
+delButton.className = "delete-btn";
+delButton.textContent = "X";
+li.append(span, delButton);
+template.content.appendChild(li);
+
+function handleDelClick() {
+    
+}
+
+function handleAddClick() {
+    if (taskInput.value === "") return;
+    const newTask = template.content.cloneNode(true);
+    newTask.querySelector(".task").textContent = taskInput.value;
+    newTask.querySelector(".delete-btn").addEventListener('click', handleDelClick);
+    ul.appendChild(newTask);
+}
+
+addButton.addEventListener('click', handleAddClick);
