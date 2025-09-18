de0c1b8b686481110cbdd133e8c06baa74c2a357
Ying Jie Mei <jackymei95@gmail.com>
2025-09-18 17:01:12 -0400

    checkForDuplicateTask

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// create elements
 const form = document.createElement("form");
 const ul = document.createElement("ul");
 const li = document.createElement("li");
@@ -8,7 +9,7 @@ const delButton = document.createElement("button");
 const taskInput = document.getElementById("userTaskInput");
 const addButton = document.getElementById("addTaskButton");
 
-// form for user input and add task button
+// form for user input field and add task button
 form.id = "addTaskForm";
 const taskForm = form;
 form.append(taskInput, addButton);
@@ -17,6 +18,7 @@ taskInput.setAttribute("maxlength", "50");
 addButton.type = "submit";
 document.body.appendChild(form);
 
+// ul container for all li(task) elements
 ul.id = "taskList";
 ul.style.listStyleType = "none";
 document.body.appendChild(ul);
@@ -29,7 +31,9 @@ delButton.textContent = "X";
 li.append(span, delButton);
 template.content.appendChild(li);
 
+// handle event on button click to delete task
 function handleDelClick(event) {
+  event.preventDefault();
   if (window.confirm("Are you sure you want to delete this task?")) {
     event.target.parentNode.remove();
     if (ul.children.length === 0) {
@@ -39,24 +43,44 @@ function handleDelClick(event) {
   taskInput.focus();
 }
 
+// handle event on form submit to add task
 function handleAddClick(event) {
-    event.preventDefault();
-    if (taskInput.value === "") {
-        window.alert("Input is empty!");
-        taskInput.focus();
-        return;
-    }
-    const newTask = template.content.cloneNode(true);
-    newTask.querySelector("li").classList.add("taskItem");
-    newTask.querySelector(".task").textContent = taskInput.value;
-    newTask.querySelector(".delete-btn").addEventListener("click", handleDelClick);
-    ul.appendChild(newTask);
-    ul.style.border = "2px solid black";  
-    ul.style.padding = "10px";
-    taskInput.value = "";
+  event.preventDefault();
+  // condition check for empty input
+  if (taskInput.value === "") {
+    window.alert("Input is empty!");
     taskInput.focus();
-}
+    return;
+  }
 
+  // find duplicate tasks by loop through li elements
+  const tasks = document.querySelectorAll(".taskItem .task");
+  let duplicateFound = false;
+  tasks.forEach((task) => {
+    if (task.textContent.toLowerCase() === taskInput.value.toLowerCase()) {
+      task.parentElement.remove();
+      duplicateFound = true;
+    }
+  });
+  if (duplicateFound) {
+    window.alert("Duplicate task found, please input another task!");
+  }
+
+  // reuse template for each new task enter by user
+  const newTask = template.content.cloneNode(true);
+  newTask.querySelector("li").classList.add("taskItem");
+  newTask.querySelector(".task").textContent = taskInput.value;
+  // add event listener for delete task
+  newTask
+    .querySelector(".delete-btn")
+    .addEventListener("click", handleDelClick);
+  ul.appendChild(newTask);
+  ul.style.border = "2px solid black";
+  ul.style.padding = "10px";
+  taskInput.value = "";
+  taskInput.focus();
+}
+// add event listener for add task
 taskForm.addEventListener("submit", handleAddClick);
 
 taskInput.focus();
